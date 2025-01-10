db95f499d94bf8933d55814f7a0d570a56868614
EduardoSRocha <eddie.maneki@gmail.com>
2025-01-10 17:11:26 -0300

    feat: configure global ValidationPipe for request validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    })
+  );
 
   const options = new DocumentBuilder()
     .setTitle('TEST HUB XP API')
