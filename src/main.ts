c0f4f920956752b224c8b24396d9d1cf3b0916c5
EduardoSRocha <eddie.maneki@gmail.com>
2025-01-10 17:08:58 -0300

    chore: set up Swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('TEST HUB XP API')
+    .setDescription('API for testing for Hub XP, providing endpoints to manage resources and operations.')
+    .setVersion('1.0')
+    .build();
+
+const document = SwaggerModule.createDocument(app, options);
+SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
