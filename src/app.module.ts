d98879ed023b7b391d505ab3127c4b4ebe70d73a
EduardoSRocha <eddie.maneki@gmail.com>
2025-01-10 18:03:55 -0300

    chore: add MongooseModule with async configuration for MongoDB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { OrdersModule } from './orders/orders.module';
 import { CategoriesModule } from './categories/categories.module';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ ProductsModule, CategoriesModule, OrdersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      load: [],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: `mongodb://${configService.get<string>('DATABASE_HOST')}:${configService.get<string>('DATABASE_PORT')}/${configService.get<string>('DATABASE_NAME')}`,
+      }),
+      inject: [ConfigService],
+    }),
+    ProductsModule,
+    CategoriesModule,
+    OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
